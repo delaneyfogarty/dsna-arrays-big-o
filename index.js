dacf88eade838369c9d74d55cddda7169d4f30cb
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-11 11:11:37 -0700

    working on unshift method

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 function push(arr, item) {
-    // arr.length = arr.length -1;
-    for(let i = 0; i < arr.length; i++);
+  // arr.length = arr.length -1;
+  for (let i = 0; i < arr.length; i++);
 
-    // arr[item] = arr [item + 1]
-    return arr.length; 
+  // arr[item] = arr [item + 1]
+  return arr.length;
 }
-// read the array length 
+// read the array length
 // set the array length +1
-// set element by index 
-module.exports = { push };
+// set element by index
+
+function unshift(arr, item) {
+  const firstSpace = arr[0];
+  // create a variable that holds onto the position we are trying to push something into
+  // loop through the array
+  // add new item
+  // return new length of array
+  for (let i = arr.length - 1; i > arr.length; i--) {
+    firstSpace.push(item);
+  }
+  return arr.length;
+}
+
+module.exports = { push, unshift };
 
 // const array = ['a', 'b', 'c'];
 // console.log (push(array, d))
